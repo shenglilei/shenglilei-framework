4c48e60b0bafa46c8059a4d72c78e08cf2ebfd3d
stevencheng <stevencheng@dofun.cn>
2021-09-17 14:56:40 +0800

    fixbug

--- uggame-framework-core/src/main/java/com/dofun/uggame/framework/core/web/WebAutoConfiguration.java
+++ uggame-framework-core/src/main/java/com/dofun/uggame/framework/core/web/WebAutoConfiguration.java
@@ -1,12 +1,20 @@
 package com.dofun.uggame.framework.core.web;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,6 +23,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Time:10:05
  */
 @Slf4j
+@EnableWebMvc
 @Configuration
 @ConditionalOnProperty(prefix = "global-exception", name = "enable", matchIfMissing = true, havingValue = "true")
 public class WebAutoConfiguration implements WebMvcConfigurer {
@@ -24,5 +33,16 @@ public class WebAutoConfiguration implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("forward:/info");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
         log.info("homepage view is ready to inject.");
+    }
+
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(jackson2HttpMessageConverter);
     }
 }
