43df4f24482b4822496f209d0639cfe4fc770620
stevencheng <stevencheng@dofun.cn>
2021-09-22 17:50:49 +0800

    fix bug

--- uggame-framework-core/src/main/java/com/dofun/uggame/framework/core/web/WebAutoConfiguration.java
+++ uggame-framework-core/src/main/java/com/dofun/uggame/framework/core/web/WebAutoConfiguration.java
@@ -40,13 +40,12 @@ public class WebAutoConfiguration implements WebMvcConfigurer {
     }
 
 
-
-        @Override
-        public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-            registry.addResourceHandler("swagger-ui.html")
-                    .addResourceLocations("classpath:/META-INF/resources/");
-            registry.addResourceHandler("/webjars/**")
-                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
