f1ac734ac96aa5e1b8116dc96e48f92b0f8309fe
stevencheng <stevencheng@dofun.cn>
2021-09-26 20:04:28 +0800

    统一处理时区

---
+++ uggame-framework-core/src/main/java/com/dofun/uggame/framework/core/constants/Constants.java
@@ -0,0 +1,16 @@
+package com.dofun.uggame.framework.core.constants;
+
+
+/**
+ * 系统常量
+ */
+public class Constants {
+    /**
+     * 系统默认时区：越南时区，比北京时间晚一小时整
+     * 示例
+     * <p>
+     * 北京时间：2021-09-26 20:08:41
+     * 越南时间：2021-09-26 19:08:41
+     */
+    public static final String SYSTEM_DEFAULT_CONFIG_TIMEZONE = "Asia/Ho_Chi_Minh";
+}
