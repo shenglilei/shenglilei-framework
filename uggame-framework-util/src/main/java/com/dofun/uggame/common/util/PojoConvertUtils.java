e4bba7a9de09942024fe8eb8467abf721fb8078d
Achin <achinliu@163.com>
2021-09-30 14:52:11 +0800

    工具类优化

--- uggame-framework-util/src/main/java/com/dofun/uggame/common/util/PojoConvertUtils.java
+++ uggame-framework-util/src/main/java/com/dofun/uggame/common/util/PojoConvertUtils.java
@@ -1,55 +1,90 @@
 package com.dofun.uggame.common.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
-import org.springframework.util.CollectionUtils;
+import org.springframework.cglib.beans.BeanCopier;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 public class PojoConvertUtils {
 
     /**
+     * BeanCopier的缓存
+     * BeanCopier效率比BeanUtils.copyProperties效率快很多，但重复create性能会降低，这里做缓存处理
+     */
+    static final ConcurrentHashMap<String, BeanCopier> BEAN_COPIER_CACHE = new ConcurrentHashMap<>();
+
+    /**
      * 赋值实体值
      *
-     * @param src
-     * @param targetClass
+     * @param source
+     * @param target
      * @param <T>
      * @return
      */
-    public static <T> T convertPojo(Object src, Class<T> targetClass) {
-        if (src == null) {
+    public static <T> T convertPojo(Object source, Class<T> target) {
+        if (source == null || target == null) return null;
+        String key = genKey(source.getClass().getName(), target.getName());
+        BeanCopier beanCopier;
+        if (BEAN_COPIER_CACHE.containsKey(key)) {
+            beanCopier = BEAN_COPIER_CACHE.get(key);
+        } else {
+            beanCopier = BeanCopier.create(source.getClass(), target, false);
+            BEAN_COPIER_CACHE.put(key, beanCopier);
+        }
+        try {
+            T t = target.newInstance();
+            beanCopier.copy(source, t, null);
+            return t;
+        } catch (Exception e) {
+            log.error("bean copy error reason is", e);
             return null;
         }
-        T instance = null;
-        try {
-            instance = targetClass.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-            log.error("convertPojo 实例化异常", e);
-        }
-        BeanUtils.copyProperties(src, instance);
-        return instance;
     }
 
     /**
      * 批量赋值实体的值
      *
-     * @param orig
-     * @param targetClass
+     * @param source
+     * @param target
      * @param <T>
      * @return
      */
-    public static <T> List<T> convertPojos(List orig, Class<T> targetClass) {
-        if (CollectionUtils.isEmpty(orig)) {
-            return Collections.emptyList();
+    public static <T> List<T> convertPojos(List source, Class<T> target) {
+        if (source == null || target == null) return null;
+        String key = genKey(source.getClass().getName() + source.get(0).getClass().getName(), target.getName());
+        BeanCopier copier;
+        if (BEAN_COPIER_CACHE.containsKey(key)) {
+            copier = BEAN_COPIER_CACHE.get(key);
+        } else {
+            copier = BeanCopier.create(source.get(0).getClass(), target, false);
+            BEAN_COPIER_CACHE.put(key, copier);
         }
-        List<T> list = new ArrayList<>(orig.size());
-        for (Object object : orig) {
-            list.add(convertPojo(object, targetClass));
+        try {
+            List<T> list = new ArrayList<>();
+            for (Object o : source) {
+                T t = target.newInstance();
+                copier.copy(o, t, null);
+                list.add(t);
+            }
+            return list;
+        } catch (Exception e) {
+            log.error("bean copy error reason is", e);
+            return null;
         }
-        return list;
     }
+
+    /**
+     * 生成key
+     *
+     * @param sourceClassName 源文件的className
+     * @param tragetClassName 目标文件的className
+     * @return string
+     */
+    private static String genKey(String sourceClassName, String tragetClassName) {
+        return sourceClassName + tragetClassName;
+    }
+
 }
