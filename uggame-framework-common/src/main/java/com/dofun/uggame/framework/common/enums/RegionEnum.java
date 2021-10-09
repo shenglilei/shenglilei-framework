411848ad22df6ae330a1cb21a7fbf0ba9f78bcd8
stevencheng <stevencheng@dofun.cn>
2021-10-09 09:45:15 +0800

    重新定义国家字段

--- uggame-framework-common/src/main/java/com/dofun/uggame/framework/common/enums/RegionEnum.java
+++ uggame-framework-common/src/main/java/com/dofun/uggame/framework/common/enums/RegionEnum.java
@@ -1,5 +1,6 @@
 package com.dofun.uggame.framework.common.enums;
 
+import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -10,50 +11,80 @@ import org.apache.commons.lang3.StringUtils;
  * Date:2021/9/30
  * Time:13:58
  */
+@Getter
 public enum RegionEnum {
     /**
      * 越南
      */
-    VIETNAM("vietnam"),
-    /**
-     * 印度尼西亚
-     */
-    INDONESIA("indonesia"),
-    /**
-     * 马来西亚
-     */
-    MALAYSIA("malaysia"),
-    /**
-     * 泰国
-     */
-    THAILAND("thailand"),
+    VIETNAM(3, "vietnam", 84),
+//    /**
+//     * 印度尼西亚
+//     */
+//    INDONESIA("indonesia",62),
+//    /**
+//     * 马来西亚
+//     */
+//    MALAYSIA("malaysia",60),
+//    /**
+//     * 泰国
+//     */
+//    THAILAND("thailand",66),
     ;
 
-    private final String name;
+    /**
+     * 国家Id
+     */
+    private final Integer countryId;
+    /**
+     * 国家名称
+     */
+    private final String countryName;
+    /**
+     * 国家区号
+     */
+    private final Integer countryCode;
 
-    RegionEnum(String name) {
-        this.name = name;
+    RegionEnum(Integer countryId, String countryName, Integer countryCode) {
+        this.countryId = countryId;
+        this.countryName = countryName;
+        this.countryCode = countryCode;
     }
 
-    public static boolean isExisted(String name) {
-        return null != forName(name);
+    public static boolean isExisted(String countryName) {
+        return null != forName(countryName);
     }
 
-    public static RegionEnum forName(String name) {
-        for (RegionEnum endpoint : RegionEnum.values()) {
-            String epName = endpoint.getName();
-            if (StringUtils.equals(name, epName)) {
-                return endpoint;
+    public static RegionEnum forId(Integer countryId) {
+        for (RegionEnum item : RegionEnum.values()) {
+            Integer id = item.getCountryCode();
+            if (countryId.equals(id)) {
+                return item;
             }
         }
         return null;
     }
 
-    public String getName() {
-        return name;
+    public static RegionEnum forName(String countryName) {
+        for (RegionEnum item : RegionEnum.values()) {
+            String name = item.getCountryName();
+            if (StringUtils.equals(countryName, name)) {
+                return item;
+            }
+        }
+        return null;
     }
 
-    public boolean equals(String name) {
-        return this.name.equals(name);
+    public static RegionEnum forCode(Integer countryCode) {
+        for (RegionEnum item : RegionEnum.values()) {
+            Integer code = item.getCountryCode();
+            if (countryCode.equals(code)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public boolean equals(String countryName) {
+        return this.getCountryName().equals(countryName);
     }
 }
