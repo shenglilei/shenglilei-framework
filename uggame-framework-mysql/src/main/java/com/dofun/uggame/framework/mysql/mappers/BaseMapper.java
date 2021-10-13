973752a609f0ff90522875efc3a4071404f2a959
stevencheng <stevencheng@dofun.cn>
2021-10-13 17:02:28 +0800

    使用tk.mybatis.mapper.common.BaseMapper

--- uggame-framework-mysql/src/main/java/com/dofun/uggame/framework/mysql/mappers/BaseMapper.java
+++ uggame-framework-mysql/src/main/java/com/dofun/uggame/framework/mysql/mappers/BaseMapper.java
@@ -5,7 +5,7 @@ import tk.mybatis.mapper.common.IdsMapper;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
-public interface BaseMapper<T extends BaseEntity> extends Mapper<T>, MySqlMapper<T>, IdsMapper<T> {
+public interface BaseMapper<T extends BaseEntity> extends tk.mybatis.mapper.common.BaseMapper<T>, Mapper<T>, MySqlMapper<T>, IdsMapper<T> {
     //TODO
     //FIXME 特别注意，该接口不能被扫描到，否则会出错
 }
